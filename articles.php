6d8fff8b132a0ef5875d6368069a1754b2c6aaf4
Caleb J Caldwell <ccaldw2@uwo.ca>
2019-01-10 23:56:11 -0500

    limited article generation and extended pagination functionality

--- articles.php
+++ articles.php
@@ -1,14 +1,20 @@
 <?php
     require_once 'DBWrapper.php';
     $db = new DBWrapper();
+    $articles_per_page = 2;
+
     $page = $_GET['page'];
-    $articles = $db -> fetch_list();
-    $articles_per_page = 5;
+    $articles = $db -> fetch_list($page, $articles_per_page);
     $len = 5;
 
     echo"<h1>All Articles</h1>";
+    $ticker = 0;
     foreach($articles as $id => $article) {
         echo "<img width='80vw' height='60vw' src='$article[ImageLink]'/> <a href='../article.php/?id=$id'>$article[Title]</a><br/><hr/>";
+        $ticker += 1;
+        if($ticker >= $articles_per_page){
+            break;
+        }
     }
 
     $last = ceil($len / $articles_per_page);
